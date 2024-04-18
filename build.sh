712752aee9a39f6d623a33acefca1031474ba79f
bm <darkshadeuz@gmail.com>
2024-04-18 12:04:32 +0500

    feat: Add build script for windows amd64 arch

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 rm -rf build
+
+# linux amd64
 env GOOS=linux GOARCH=amd64 go build -o build/linux_amd64 main.go
+
+# windows amd64
+env GOOS=windows GOARCH=amd64 go build -o build/windows_amd64.exe main.go
